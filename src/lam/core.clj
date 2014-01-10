86472fe08135729174b873cca60260f81c0acc61
Oregu <oleg.burykin@gmail.com>
2014-01-10 22:13:29 +0400

    Church numbers approve some working

--- src/lam/core.clj
+++ src/lam/core.clj
@@ -2,111 +2,53 @@
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic]))
 
-(defn noo [tag u] (predc u (fn [x] (clojure.core/not= (if (seq? x) (first x) x) tag))))
 (defn symbolo [x] (predc x symbol?))
-(defn not-symbolo [x] (predc x #(not (symbol? %))))
-(defn numbero [x] (predc x number?))
+(defn not-fno [x] (predc x (fn [x] (not (and (seq? x) (= (first x) 'fn))))))
 
-(declare not-in-envo)
-(declare proper-listo)
+(declare substo)
 
-(defn lookupo [x env t]
-  (fresh [rest y v]
-    (conso `(~y ~v) rest env)
-    (conde
-      [(== y x) (== v t)]
-      [(!= y x) (lookupo x rest t)])))
-
-(declare eval-expo)
-
-(defn substo [body vr vl body-ex]
+(defn eval-expo [exp val]
   (conde
-    [(symbolo body)
-     (== body vr)
-     (== body-ex vl)]
-    [(symbolo body)
-     (!= body vr)
-     (== body body-ex)]
-    [(numbero body)
-     (== body body-ex)]
-    [(fresh [rator rand rator-ex rand-ex]
-      (== `(~rator ~rand) body)
-      (substo rator vr vl rator-ex)
-      (substo rand vr vl rand-ex)
-      (eval-expo `(~rator-ex ~rand-ex) '() body-ex))]
-    [(fresh [x body2 body2-ex]
-      (== `(~'fn [~x] ~body2) body)
-      (!= x vr)
-      (substo body2 vr vl body2-ex) ;; TODO do not substitute bound variables
-      (== body-ex `(~'fn [~x] ~body2-ex)))]))
-
-(defn reduceo [body env body-ex] ;; TODO trim environment
-  (conde
-    [(emptyo env)
-     (== body body-ex)]
-    [(fresh [vr vl env-t body2]
-      (conso `(~vr ~vl) env-t env)
-      (substo body vr vl body2)
-      (reduceo body2 env-t body-ex))]))
-
-(defn eval-expo [exp env val]
-  (conde
-    [(fresh [v]
-      (== `(~'quote ~v) exp)
-      (not-in-envo 'quote env)
-      (noo 'closure v)
-      (== v val))]
-    [(fresh [a*]
-      (conso 'list a* exp)
-      (not-in-envo 'list env)
-      (noo 'closure a*)
-      (proper-listo a* env val))]
     [(symbolo exp)
-      (lookupo exp env val)]
-    [(symbolo exp)
-      (lookupo exp `((~exp ~exp)) val)]
-    [(fresh [rator rand x body env- a env2]
+     (== val exp)]
+    [(fresh [rator rand r2 x body body2]
       (== `(~rator ~rand) exp)
-      (not-symbolo rator)
-      (eval-expo rator env `(~'closure ~x ~body ~env-))
-      (eval-expo rand env a)
-      (conso `(~x ~a) env- env2)
-      (eval-expo body env2 val))]
-    [(fresh [rator rand x v2]
+      (eval-expo rator `(~'fn [~x] ~body))
+      (eval-expo rand r2)
+      (substo body x r2 body2)
+      (eval-expo body2 val))]
+    [(fresh [rator rand r1 r2]
       (== `(~rator ~rand) exp)
-      (not-symbolo rator)
-      (eval-expo rator env v2)
-      (noo 'closure v2)
-      (reduceo exp env val))]
-    [(fresh [x body body-ex env2]
+      (not-fno rator)
+      (eval-expo rator r1)
+      (eval-expo rand r2)
+      (== val `(~r1 ~r2)))]
+    [(fresh [x body body-ex]
       (== `(~'fn [~x] ~body) exp)
       (symbolo x)
-      (not-in-envo 'fn env)
-      (reduceo body env body-ex)
-      (== `(~'closure ~x ~body-ex ~env) val))]
-    [(fresh [rator rand]
+      (trace-lvars "lam" [x body])
+      (eval-expo body body-ex)
+      (== `(~'fn [~x] ~body-ex) val))]))
+
+(defn substo [exp vr vl body-ex]
+  (conde
+    [(symbolo exp)
+     (== exp vr)
+     (== vl body-ex)]
+    [(symbolo exp)
+     (!= exp vr)
+     (== exp body-ex)]
+    [(fresh [rator rand rator-ex rand-ex]
       (== `(~rator ~rand) exp)
-      (symbolo rator)
-      (reduceo exp env val))]
-    [(fresh [x body env2]
-      (== `(~'closure ~x ~body ~env2) exp)
-      (symbolo x)
-      (== exp val))]))
-
-(defn not-in-envo [x env]
-  (conde
-    [(fresh [y v rest]
-      (conso `(~y ~v) rest env)
-      (!= y x)
-      (not-in-envo x rest))]
-    [(== '() env)]))
-
-(defn proper-listo [exp env val]
-  (conde
-    [(== '() exp)
-     (== '() val)]
-    [(fresh [a d t-a t-d]
-       (conso a d exp)
-       (conso t-a t-d val)
-       (eval-expo a env t-a)
-       (proper-listo d env t-d))]))
+      (substo rator vr vl rator-ex)
+      (substo rand vr vl rand-ex)
+      (eval-expo `(~rator-ex ~rand-ex) body-ex))]
+    [(fresh [x body body2-ex]
+      (== `(~'fn [~x] ~body) exp)
+      (!= x vr)
+      (substo body vr vl body2-ex)
+      (== body-ex `(~'fn [~x] ~body2-ex)))]
+    [(fresh [x body]
+      (== `(~'fn [~x] ~body) exp)
+      (== x vr)
+      (== exp body-ex))]))
--- src/lam/lamcalc.clj
+++ src/lam/lamcalc.clj
@@ -6,8 +6,5 @@
 (def lam-zero '(fn [f] (fn [x] x)))
 (def lam-succ '(fn [n] (fn [f] (fn [x] (f ((n f) x))))))
 
-(def lam-one
-  (first (run 1 [q] (eval-expo `(~lam-succ ~lam-zero) '() q))))
-
-(def lam-two
-  (first (run 1 [q] (eval-expo `(~lam-succ ~lam-one) '() q))))
+(def lam-one (first (run 1 [q] (eval-expo `(~lam-succ ~lam-zero) q))))
+(def lam-two (first (run 1 [q] (eval-expo `(~lam-succ ~lam-one) q))))
