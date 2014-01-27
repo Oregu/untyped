226fdf4ff72851053b5e838eef3a562187cbf7ae
Oregu <oleg.burykin@gmail.com>
2014-01-27 22:22:33 +0400

    Trying capture avoiding substitution (with no luck)

--- README.md
+++ README.md
@@ -33,10 +33,15 @@ For number four it used just half a second, but generating Church 5 takes 20 sec
 :- (!= (_1 f)) #<core$symbol_QMARK_ clojure.core$symbol_QMARK_@7116778b> (!= (_1 n)) (!= (_0 _1)) #<core$not_fn_QMARK_ untyped.core$not_fn_QMARK_@62a49a92>)
 ```
 
-Currently, I can't wait it to generate successor function.
+Currently, it hangs for too long while generating successor function.
 
 Future work
 -----------
 - No explicit parentheses
 - No Clojure syntax (lambdas)
 - Run backwards (generate combinators, for example 'eater' Fx=F, if one exist)
+
+Notes
+-----
+“This final version of appendo illustrates an important principle: unifications should always come before recursive calls, or calls to other “serious” relations”
+- From WIll Byrd's “Relational Programming in miniKanren: Techniques, Applications, and Implementations”
--- src/untyped/core.clj
+++ src/untyped/core.clj
@@ -11,6 +11,40 @@
 (defn not-fno [x] (predc x not-fn?))
 (defn symbolo [x] (predc x symbol?))
 
+(defn excludo [x l o]
+  (conde
+    [(emptyo l)(emptyo o)]
+    [(fresh [h t t1]
+      (conso h t l)
+      (!= h x)
+      (excludo x t t1)
+      (conso h t1 o))]
+    [(fresh [t]
+      (conso x t l)
+      (excludo x t o))]))
+
+(defn not-in-listo [x l]
+  (conde
+    [(emptyo l)]
+    [(fresh [h t]
+      (conso h t l)
+      (!= h x)
+      (not-in-listo x t))]))
+
+(defn free-varo [exp fv]
+  (conde
+    [(symbolo exp)
+     (conso exp '() fv)]
+    [(fresh [x body fv-f]
+      (== `(~'fn [~x] ~body) exp)
+      (free-varo body fv-f)
+      (excludo x fv-f fv))]
+    [(fresh [rator rand fv1 fv2]
+      (== `(~rator ~rand) exp)
+      (free-varo rator fv1)
+      (free-varo rand fv2)
+      (appendo fv1 fv2 fv))]))
+
 (defn substo [exp x v subexp]
   (conde
     [(symbolo exp)
@@ -19,8 +53,10 @@
     [(symbolo exp)
      (!= exp x)
      (== exp subexp)]
-    [(fresh [arg body subbody]
+    [(fresh [arg body subbody fv]
       (== `(~'fn [~arg] ~body) exp)
+      ; (free-varo v fv)
+      ; (not-in-listo arg fv)
       (substo body x v subbody)
       (== `(~'fn [~arg] ~subbody) subexp))]
     [(fresh [rator rand r1 r2]
@@ -32,7 +68,7 @@
           (== r1 `(~'fn [~x] ~body))
           (substo body x r2 subexp))]
         [(not-fno r1)
-         (== `(~r1 ~r2) subexp)]))]))
+          (== `(~r1 ~r2) subexp)]))]))
 
 (defn eval-expo [exp val]
   (conde
