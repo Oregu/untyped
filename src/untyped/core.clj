5e6f08f2314c23b53164bee4fe39f1eabf45b778
Oregu <oleg.burykin@gmail.com>
2014-02-23 14:54:04 +0400

    Using @namin code to bootstrap evaluation w/ nominal logic

--- src/untyped/core.clj
+++ src/untyped/core.clj
@@ -12,8 +12,48 @@
 (defn not-fno [x] (predc x not-fn? 'not-fn?))
 (defn symbolo [x] (predc x symbol? 'symbol?))
 
-(defn lam  [x e] `(~'fn ~(nom/tie x e)))
-(defn lamo [x e o] (== o (lam x e)))
+(defn reifier-for [tag x]
+  (fn [c v r a]
+    (let [x (walk* r (walk* a x))]
+      (when (symbol? x)
+        `(~tag ~x)))))
+
+(defn lam [x e] `(~'fn ~(nom/tie x e)))
+(defn app [e1 e2] `(~e1 ~e2))
+(defn lamo [x e out] (== out (lam x e)))
+(defn appo [e1 e2 out] (all (== out (app e1 e2)) (!= e1 'fn)))
+(defn nomo [x] (predc x nom? (reifier-for 'nom x)))
+
+(defn namin-substo [e new a out] ;; out == [new/a]e
+  (conde
+    [(nomo e) (== e a) (== new out)]
+    [(nomo e) (!= e a) (== e out)]
+    [(fresh [e1 e2 o1 o2]
+       (appo e1 e2 e)
+       (appo o1 o2 out)
+       (namin-substo e1 new a o1)
+       (namin-substo e2 new a o2))]
+    [(fresh [e0 o0]
+       (nom/fresh [c]
+         (lamo c e0 e)
+         (lamo c o0 out)
+         (nom/hash c a) ;; [new/c]λc.e ≡α λc.e
+         (nom/hash c new) ;; [c/a]λc.a ≡α λa.c ≢α λc.c
+         (namin-substo e0 new a o0)))]))
+
+(defn namin-eval-expo [exp val]
+  (conde
+    [(fresh [body]
+      (nom/fresh [x]
+        (lamo x body exp)
+        (trace-lvars "lamo" [x body exp val])
+        (== exp val)))]
+    [(fresh [rator rand body]
+      (nom/fresh [x]
+        (appo rator rand exp)
+        (trace-lvars "appo" [rator rand])
+        (lamo x body rator)
+        (namin-substo body rand x val)))]))
 
 (defn substo [exp x v subexp]
   (conde
