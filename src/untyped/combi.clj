abed00f5c0c556774c1bc4dbfd7de03afa068199
Oregu <oleg.burykin@gmail.com>
2014-03-04 23:31:47 +0400

    Something is very wrong about SKK :(

--- src/untyped/combi.clj
+++ src/untyped/combi.clj
@@ -7,6 +7,10 @@
 (defn S [x y z] (lam x (lam y (lam z (app (app x z) (app y z))))))
 (defn K [x y] (lam x (lam y x)))
 
+(defn SKK []
+  (time (first (run 1 [q] (nom/fresh [x y xx yy z]
+    (eval-expo (S (K x y) (K xx yy) z) q))))))
+
 (defn gen-eater [] ;; Also called K-infinity
   (time (first (run 1 [q] (nom/fresh [x]
     (eval-expo (app q x) q))))))
