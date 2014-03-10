e05ba41ead79963d27975d6d60821f30d54d17c5
Oregu <oleg.burykin@gmail.com>
2014-03-10 18:38:20 +0400

    Solving combinator exercices

--- src/untyped/combi.clj
+++ src/untyped/combi.clj
@@ -6,6 +6,19 @@
 
 (defn S [x y z] (lam x (lam y (lam z (app (app x z) (app y z))))))
 (defn K [x y]   (lam x (lam y x)))
+(defn I [x]     (lam x x))
+
+(defn gen-ex2-9i []
+  (time (first (run 1 [q] (nom/fresh [x a]
+    (eval-expo (app q x) (app x (I a))))))))
+
+(defn gen-ex2-9ii []
+  (time (first (run 1 [q] (nom/fresh [x y a]
+    (eval-expo (app (app q x) y) (app (app x (I a)) y)))))))
+
+; (defn gen-2-10iii []
+;   (time (first (run 1 [q] (nom/fresh [x y a]
+;     (eval-expo (app (app q (I x)) y) (app (app x (I a)) y)))))))
 
 (defn gen-eater [] ;; Also called K-infinity
   (time (first (run 1 [q] (nom/fresh [x]
