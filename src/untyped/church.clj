f75af79566c80993802aea22dcacce245d17ec0a
Oregu <oleg.burykin@gmail.com>
2014-03-16 14:38:45 +0400

    Iterate to DRY

--- src/untyped/church.clj
+++ src/untyped/church.clj
@@ -4,43 +4,39 @@
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom]
         [untyped.core]))
 
-(defn ch0 [f x] (lam f (lam x x)))
-(defn ch1 [f x] (lam f (lam x (app f x))))
-(defn ch2 [f x] (lam f (lam x (app f (app f x)))))
-(defn ch3 [f x] (lam f (lam x (app f (app f (app f x))))))
-(defn ch4 [f x] (lam f (lam x (app f (app f (app f (app f x)))))))
-(defn ch5 [f x] (lam f (lam x (app f (app f (app f (app f (app f x))))))))
-(defn ch6 [f x] (lam f (lam x (app f (app f (app f (app f (app f (app f x)))))))))
+(defn ch [n f x]
+  "Produce Church numeral n with noms f and x"
+  (lam f (lam x (nth (iterate (partial app f) x) n))))
+
 (defn ch-succ [n f x] (lam n (lam f (lam x (app f (app (app n f) x))))))
 (defn ch+ [m n f x] (lam m (lam n (lam f (lam x (app (app m f) (app (app n f) x)))))))
 
-(defn gen-ch3 []    ;; 898 ms
+(defn gen-ch3 []    ;; 785 ms
   (time (first (run 1 [q] (nom/fresh [n f x f1 x1] (eval-expo
     (app (ch-succ n f x) q) ; succ ? =
-    (ch4 f1 x1)))))))       ; 4
+    (ch 4 f1 x1)))))))  ; 4
 
-(defn gen-ch5 []    ;; 36 s
+(defn gen-ch5 []    ;; 31 s
   (time (first (run 1 [q] (nom/fresh [n f x f1 x1] (eval-expo
     (app (ch-succ n f x) q) ; succ ? =
-    (ch6 f1 x1)))))))       ; 6
+    (ch 6 f1 x1)))))))  ; 6
 
-(defn gen-ch-succ [] ;; 42 s
+(defn gen-ch-succ [] ;; 41 s
   (time (first (run 1 [q]
     (nom/fresh [f x f1 x1]
-      (eval-expo (app q (ch0 f x)) (ch1 f1 x1)))     ; q? 0 = 1
+      (eval-expo (app q (ch 0 f x)) (ch 1 f1 x1)))     ; q? 0 = 1
     (nom/fresh [f x f1 x1]
-      (eval-expo (app q (ch1 f x)) (ch2 f1 x1))))))) ; q? 1 = 2
+      (eval-expo (app q (ch 1 f x)) (ch 2 f1 x1))))))) ; q? 1 = 2
 
-;; NOT working. Overflows
+;; NOT working. Out of memory after 15 mins
 (defn gen-ch+ []
   (time (first (run 1 [q]
     (nom/fresh [f x f1 x1 f2 x2]
-      (eval-expo (app (app q (ch2 f x)) (ch3 f1 x1)) ; q? 2 3 =
-                 (ch5 f2 x2)))))))                   ; 5
-
-;; Still overflows
-(defn gen-ch+-2 []
-  (time (first (run 1 [q] (fresh [s2]
+      (eval-expo (app (app q (ch 1 f x)) (ch 1 f1 x1)) ; q? 1 1 =
+                 (ch 2 f2 x2)))                       ; 2
     (nom/fresh [f x f1 x1 f2 x2]
-      (eval-expo (app q (ch2 f x)) s2)
-      (eval-expo (app s2 (ch3 f1 x1)) (ch5 f2 x2))))))))
+      (eval-expo (app (app q (ch 1 f x)) (ch 2 f1 x1)) ; q? 1 2 =
+                 (ch 3 f2 x2)))                       ; 3
+    (nom/fresh [f x f1 x1 f2 x2]
+      (eval-expo (app (app q (ch 2 f x)) (ch 2 f1 x1)) ; q? 2 2 =
+                 (ch 4 f2 x2)))))))                   ; 4
--- test/untyped/test/church.clj
+++ test/untyped/test/church.clj
@@ -8,14 +8,14 @@
 
 (deftest eval-succ
   (is (first (run 1 [q] (nom/fresh [n f x f1 x1]
-    (eval-expo (app (ch-succ n f x) (ch0 f x))
-               (ch1 f1 x1))))))
+    (eval-expo (app (ch-succ n f x) (ch 0 f x))
+               (ch 1 f1 x1))))))
   (is (first (run 1 [q] (nom/fresh [n f x f1 x1]
-    (eval-expo (app (ch-succ n f x) (ch1 f1 x1))
-               (ch2 f x)))))))
+    (eval-expo (app (ch-succ n f x) (ch 1 f1 x1))
+               (ch 2 f x)))))))
 
 (deftest eval-plus
   (is (first (run 1 [q] (nom/fresh [m n f x f1 x1 f2 x3]
-    (eval-expo (app (app (ch+ m n f x) (ch2 f2 x1)) (ch1 f1 x1)) (ch3 f x3))))))
+    (eval-expo (app (app (ch+ m n f x) (ch 2 f2 x1)) (ch 1 f1 x1)) (ch 3 f x3))))))
   (is (first (run 1 [q] (nom/fresh [m n f x]
-    (eval-expo (app (app (ch+ m n f x) (ch3 f x)) (ch2 f x)) (ch5 f x)))))))
+    (eval-expo (app (app (ch+ m n f x) (ch 3 f x)) (ch 2 f x)) (ch 5 f x)))))))
